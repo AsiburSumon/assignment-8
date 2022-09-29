b2d759653a5bb2c8ac9423417e7399592a040e90
AsiburSumon <sumonasif68@gmail.com>
2022-09-29 13:32:05 +0600

    task design updated

---
+++ src/components/Task/Activities/Activities.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import image from '../../../images/img1.jpg';
+import './Activities.css'
+
+const Activities = () => {
+    return (
+        <div>
+            <div className='profile'>
+                    <img src={image} alt="error" />
+                        <div>
+                            <h4 style={{margin:"0", marginTop:"10px"}}>Asibur Rahman Sumon</h4>
+                            <p style={{marginTop:"0"}}>Amin-Bazar,Dhaka</p>
+                        </div>
+                </div>
+                <div className='profile-info'>
+                    <h4>74<small>kg</small></h4>
+                    <h4>5'8</h4>
+                    <h4>20<small>yr</small></h4>
+                    <p>Weight</p>
+                    <p>Height</p>
+                    <p>Age</p>
+                </div>
+                <div className='break'>
+                    <h2>Add a break</h2>
+                    <div className="break-info">
+                        <h4>30s</h4>
+                        <h4>40s</h4>
+                        <h4>45s</h4>
+                        <h4>50s</h4>
+                    </div>
+                </div>
+                <div className="study-details">
+                    <h2>Study Details</h2>
+                    <div className='study-time'>
+                        <h4>Study Time: <small>0</small> second</h4>
+                    </div>
+                    <div className='break-time'>
+                        <h4>Break Time: <small>0</small> second</h4>
+                    </div>
+                    <div>
+                        <button className="activities-btn">Activity Completed</button>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default Activities;
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Task.css';
-import image from '../../images/img1.jpg'
 import Book from './Book/Book';
+import Activities from './Activities/Activities';
 
 const Task = () => {
     const [books, setBooks] = useState([]);
@@ -22,42 +22,7 @@ const Task = () => {
                 </div>
             </div>
             <div className='activities'>
-                <div className='profile'>
-                    <img src={image} alt="error" />
-                        <div>
-                            <h4 style={{margin:"0", marginTop:"10px"}}>Asibur Rahman Sumon</h4>
-                            <p style={{marginTop:"0"}}>Amin-Bazar,Dhaka</p>
-                        </div>
-                </div>
-                <div className='profile-info'>
-                    <h4>74<small>kg</small></h4>
-                    <h4>5'8</h4>
-                    <h4>20<small>yr</small></h4>
-                    <p>Weight</p>
-                    <p>Height</p>
-                    <p>Age</p>
-                </div>
-                <div className='break'>
-                    <h2>Add a break</h2>
-                    <div className="break-info">
-                        <h4>30s</h4>
-                        <h4>40s</h4>
-                        <h4>45s</h4>
-                        <h4>50s</h4>
-                    </div>
-                </div>
-                <div className="study-details">
-                    <h2>Study Details</h2>
-                    <div className='study-time'>
-                        <h4>Study Time: <small>0</small> second</h4>
-                    </div>
-                    <div className='break-time'>
-                        <h4>Break Time: <small>0</small> second</h4>
-                    </div>
-                    <div>
-                        <button className="activities-btn">Activity Completed</button>
-                    </div>
-                </div>
+                <Activities></Activities>
             </div>
         </div>
     );
